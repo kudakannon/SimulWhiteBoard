801ec76c912b14a9179a68dc4151a3d7b851d104
Nathan <Nathan@giy.com>
2021-03-26 06:14:17 +1000

    functioning divtest

--- Simula/simul/simulphase2/pages/divtest.vue
+++ Simula/simul/simulphase2/pages/divtest.vue
@@ -3,7 +3,7 @@
         <div id="whiteboardframe" onmousedown="moveWhiteboard(event)">
 
             <div id="toolbar" onmousedown="stopEvent(event)">
-                <div class="toolselector" onclick="divtest()">Reset</div>
+                <div class="toolselector" v-on:click="divtest">Reset</div>
                 <div class="toolselector">PST</div>
                 <div class="toolselector">PEN</div>
                 <div class="toolselector">PNCL</div>
@@ -13,168 +13,179 @@
             </div>
             
         </div>
+        <script type="text/javascript" charset="UTF-8">
+            var pos = { top: 0, left: 0, x: 0, y: 0 };
+            function moveWhiteboard() {
+                //look grabbed, don't highlight text
+                var wframe = document.getElementById('whiteboardframe');
+                wframe.style.cursor = 'grabbing';
+                wframe.style.userSelect = 'none';
+
+                //store current mouse and scroll postions
+                pos = {
+                left: wframe.scrollLeft,
+                top: wframe.scrollTop,
+                x: event.clientX,
+                y: event.clientY,
+                };
+
+                //
+                document.addEventListener('mousemove', dragWhiteboard);
+                document.addEventListener('mouseup', releaseWhiteboard);
+            };
+
+            function dragWhiteboard() {
+                var wframe = document.getElementById('whiteboardframe');
+                var xMove = event.clientX - pos.x;
+                var yMove = event.clientY - pos.y;
+                
+                wframe.scrollTop = pos.top - yMove;
+                wframe.scrollLeft = pos.left - xMove;
+
+
+            };
+
+            function releaseWhiteboard() {
+                var wframe = document.getElementById('whiteboardframe');
+                wframe.style.cursor = 'grab';
+                wframe.style.removeProperty('userSelect');
+
+                document.removeEventListener('mousemove', dragWhiteboard);
+                document.removeEventListener('mouseup', releaseWhiteboard);
+
+            };
+
+            function stopEvent() {
+                event.stopPropagation();
+            };
+        
+            function drag() {
+                //stopEvent(event);
+                event.dataTransfer.setData('text', event.target.id);
+                var sty = getComputedStyle(event.target);
+        
+                pos = {
+                left: parseInt(sty.left),
+                top: parseInt(sty.top),
+                x: event.clientX,
+                y: event.clientY,
+                };
+            };
+        
+            function drop() {
+                event.preventDefault();
+                var dropid = event.dataTransfer.getData('text');
+                var dropelem = document.getElementById(dropid);
+                var xMove = event.clientX - pos.x;
+                var yMove = event.clientY - pos.y;
+
+                dropelem.style.top = Math.max(0, pos.top + yMove) + 'px';
+                dropelem.style.left = Math.max(0, pos.left + xMove) + 'px';
+            };
+        
+            function allowdrop() {
+                event.preventDefault();
+            };
+        
+        </script>
     </div>	
 </template>
 
 <script>
-export default {
-data() {
-    return {
-        pos: { top: 0, left: 0, x: 0, y: 0 }
-    };
-},
-methods: {
-
-    //holds the postion values for dragging the whiteboard screen.
-
-    divtest() {
-        var wframe = document.getElementById('whiteboardframe');
-        wframe.scrollTop = 0;
-        wframe.scrollLeft = 0;
-        var wboard = document.getElementById('whiteboard');
-        wboard.innerHTML = "";
-
-        xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-
-            buildWhiteboard(JSON.parse(this.responseText));
+    export default {
+        data() {
+            return {
+                pos: { top: 0, left: 0, x: 0, y: 0 }
             };
-        };
+        },
+        mounted: function() {
+            this.$nextTick(function() {
+                this.divtest();
+            });
+        },
+        methods: {
+
+            //holds the postion values for dragging the whiteboard screen.
+
+            divtest() {
 
 
-        xmlhttp.open("GET", "../static/testdata.json", true);
-        xmlhttp.setRequestHeader("Content-type", "application/json");
-        xmlhttp.send();
-    },
+                function buildWhiteboard(wdata) {
+                    var wboard = document.getElementById('whiteboard');
+                    wboard.style.height = wdata.height
+                    wboard.style.width = wdata.width
 
-    buildWhiteboard(wdata) {
-        var wboard = document.getElementById('whiteboard');
-        wboard.style.height = wdata.height
-        wboard.style.width = wdata.width
+                    for (var x in wdata.elements) {
+                        if (wdata.elements[x].type == "postit") {
+                            buildPostit(wdata.elements[x]);
+                        };
+                    };
+                };
+                
+                function buildPostit(postdata) {
+                    var newpostit = document.createElement("div");
+                    newpostit.onmousedown = stopEvent;
+                    newpostit.ondragstart = drag;
+                    newpostit.draggable = true;
 
-        for (x in wdata.elements) {
-            if (wdata.elements[x].type == "postit") {
-                buildPostit(wdata.elements[x]);
-            };
-        };
-    },
+                    newpostit.classList.add(postdata.type);
+                    newpostit.id = postdata.id;
+                    newpostit.style.top = postdata.top;
+                    newpostit.style.left = postdata.left;
+                    newpostit.style.height = postdata.height;
+                    newpostit.style.width = postdata.width;
 
-    buildPostit(postdata) {
-        var newpostit = document.createElement("div");
-        newpostit.onmousedown = stopEvent;
-        newpostit.ondragstart = drag;
-        newpostit.draggable = true;
-
-        newpostit.classList.add(postdata.type);
-        newpostit.id = postdata.id;
-        newpostit.style.top = postdata.top;
-        newpostit.style.left = postdata.left;
-        newpostit.style.height = postdata.height;
-        newpostit.style.width = postdata.width;
-
-        var newtitle = document.createElement("div");
-        newtitle.innerHTML = postdata.title;
-        newtitle.classList.add("titlebox");
-        newpostit.appendChild(newtitle);
+                    var newtitle = document.createElement("textarea");
+                    newtitle.innerHTML = postdata.title;
+                    newtitle.classList.add("titlebox");
+                    newpostit.appendChild(newtitle);
 
 
-        var newtext = document.createElement("div");
-        newtext.innerHTML = postdata.text;
-        newtext.classList.add("textbox");
-        newpostit.appendChild(newtext);
+                    var newtext = document.createElement("textarea");
+                    newtext.innerHTML = postdata.text;
+                    newtext.classList.add("textbox");
+                    newpostit.appendChild(newtext);
 
-        document.getElementById('whiteboard').appendChild(newpostit);
+                    document.getElementById('whiteboard').appendChild(newpostit);
 
-    },
-
-    moveWhiteboard() {
-        //look grabbed, don't highlight text
-        var wframe = document.getElementById('whiteboardframe');
-        wframe.style.cursor = 'grabbing';
-        wframe.style.userSelect = 'none';
-
-        //store current mouse and scroll postions
-        pos = {
-        left: wframe.scrollLeft,
-        top: wframe.scrollTop,
-        x: event.clientX,
-        y: event.clientY,
-        };
-
-        //
-        document.addEventListener('mousemove', dragWhiteboard);
-        document.addEventListener('mouseup', releaseWhiteboard);
-    },
-
-    dragWhiteboard() {
-        var wframe = document.getElementById('whiteboardframe');
-        var xMove = event.clientX - pos.x;
-        var yMove = event.clientY - pos.y;
-        
-        wframe.scrollTop = pos.top - yMove;
-        wframe.scrollLeft = pos.left - xMove;
+                };
 
 
-    },
 
-    releaseWhiteboard() {
-        var wframe = document.getElementById('whiteboardframe');
-        wframe.style.cursor = 'grab';
-        wframe.style.removeProperty('userSelect');
 
-        document.removeEventListener('mousemove', dragWhiteboard);
-        document.removeEventListener('mouseup', releaseWhiteboard);
 
-    },
+                var wframe = document.getElementById('whiteboardframe');
+                wframe.scrollTop = 0;
+                wframe.scrollLeft = 0;
+                var wboard = document.getElementById('whiteboard');
+                wboard.innerHTML = "";
 
-    stopEvent() {
-        event.stopPropagation();
-    },
+                var xmlhttp = new XMLHttpRequest();
+                xmlhttp.onreadystatechange = function() {
+                    if (this.readyState == 4 && this.status == 200) {
 
-    drag() {
-        //stopEvent(event);
-        event.dataTransfer.setData("text/plain", event.target.id);
-        var sty = getComputedStyle(event.target);
+                        buildWhiteboard(JSON.parse(this.responseText));
+                    };
+                };
 
-        pos = {
-        left: parseInt(sty.left),
-        top: parseInt(sty.top),
-        x: event.clientX,
-        y: event.clientY,
-        };
-    },
 
-    drop() {
-        event.preventDefault();
-        var dropid = event.dataTransfer.getData("text");
-        var dropelem = document.getElementById(dropid);
-        var xMove = event.clientX - pos.x;
-        var yMove = event.clientY - pos.y;
-        
-        dropelem.style.top = Math.max(0, pos.top + yMove);
-        dropelem.style.left = Math.max(0, pos.left + xMove);
-    },
+                xmlhttp.open("GET", "./testdata.json", true);
+                xmlhttp.setRequestHeader("Content-type", "application/json");
+                xmlhttp.send();
+            },
 
-    allowdrop() {
-        event.preventDefault();
+            
+
+        }   
     }
-
-    //divtest();
-}
-}
-
-function test() {
-    console.log("test");
-}
 </script>
 
-<style scoped>
+<style>
     /*moves with the rest of site*/
     #wframecontainer {
         position: relative;
         width: 100%;
-        height: 100%;
+        height: 640px;
     }
 
     /*size of visible part of whiteboard*/
@@ -247,18 +258,26 @@ function test() {
         border-color: black;
         overflow: hidden;
         padding: 5px;
+        resize: both;
     }
 
-    div.titlebox {
-        display: block;
+    textarea.titlebox {
+        display: inline-block;
+        width: 100%;
+        height: 25px;
         background-color: yellow;
         border: 1px solid black;
         cursor: default;
+        resize: none;
     }
 
-    div.textbox {
-        display: block;
+    textarea.textbox {
+        display: inline-block;
+        width: 100%;
+        height: 80%;
         cursor: default;
+        overflow-y: scroll;
+        resize: none;
     }
 
 
