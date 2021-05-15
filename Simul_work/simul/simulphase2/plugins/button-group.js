c055dd2aa44b9175009fc25dfef9f28242d21bcd
Stephen Horsburgh <50394126+kudakannon@users.noreply.github.com>
2021-05-15 13:41:55 +1000

    Delete Simul_work directory

--- Simul_work/simul/simulphase2/plugins/button-group.js
+++
@@ -1,6 +0,0 @@
-import { ButtonGroupPlugin } from 'bootstrap-vue'
-import { ButtonPlugin } from 'bootstrap-vue'
-import Vue from 'vue'
-
-Vue.use('b-button', ButtonPlugin)
-Vue.use('b-button-group', ButtonGroupPlugin)
